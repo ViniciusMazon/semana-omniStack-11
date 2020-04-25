17cad3fcd627a41b49a4abcf7655c9a9aa5f607d
Vinicius Mazon <vnpmazon@gmail.com>
2020-04-25 17:18:00 -0300

    :rocket: Frontend: Add page to register new incident

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
         <Route exact path="/" component={Logon} />
         <Route path="/register" component={Register} />
         <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
       </Switch>
     </Router>
   );
