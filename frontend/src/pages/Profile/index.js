9ec7193696542b479fce7a4fd76801794f0bdfdd
Vinicius Mazon <vnpmazon@gmail.com>
2020-04-25 17:05:06 -0300

    :rocket: Frontend: Add profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css';
+import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
   return (
-    <h1>Profile</h1>
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be the hero logo" />
+        <span>Bem vinda, APAD</span>
+
+        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+        <button type="button">
+          <FiPower size={18} color={"#E02041"} />
+        </button>
+      </header>
+
+      <h1>Casos Cadastrados</h1>
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>valor teste</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color={"#A8A8B3"} />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>valor teste</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color={"#A8A8B3"} />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>valor teste</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color={"#A8A8B3"} />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>valor teste</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color={"#A8A8B3"} />
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
