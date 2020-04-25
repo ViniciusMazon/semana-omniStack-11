11d14f17e4740463bcaa9668d92b64cd7492dbf2
Vinicius Mazon <vnpmazon@gmail.com>
2020-04-25 16:46:48 -0300

    :rocket: Frontend: Add registration page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -15,10 +16,10 @@ export default function Logon() {
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
-          <a href="/register">
+          <Link to="/register" className="back-link">
             <FiLogIn size={16} color={"#E02041"} />
             Não tenho cadastro
-            </a>
+            </Link>
         </form>
 
       </section>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Logon} />
+        <Route exact path="/" component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </Router>
   );
