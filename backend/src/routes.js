6d5274712b4d63b22f3733f3c4d4c414b97df185
Vinicius Mazon <vnpmazon@gmail.com>
2020-04-27 10:24:36 -0300

    :rocket: Backend: Adds validation to requests

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const routes = require('express').Router();
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const session = require('./controllers/SessionController');
 const ping = require('./controllers/PingController');
@@ -6,17 +7,54 @@ const ong = require('./controllers/OngController');
 const incident = require('./controllers/IncidentController');
 const profile = require('./controllers/ProfileController');
 
-routes.post('/sessions', session.store);
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}),session.store);
+
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), ong.store);
+routes.get('/ongs', ong.index);
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required().positive()
+  })
+}), incident.store);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), incident.index);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), incident.destroy);
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}),profile.index);
 
 routes.get('/ping', ping.index);
 
-routes.post('/ongs', ong.store);
-routes.get('/ongs', ong.index);
-
-routes.post('/incidents', incident.store);
-routes.get('/incidents', incident.index);
-routes.delete('/incidents/:id', incident.destroy);
-
-routes.get('/profile', profile.index);
-
 module.exports = routes;
+
